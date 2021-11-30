6c37acadcbc55678cab236ea027abd610ffce341
Amehra2 <akashmehra2001@hotmail.com>
2021-11-30 15:58:05 +0000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import MedicalTreatmentPanel from "./MedicalTreatmentPanel";
 
 export default function App() {
   return (
+
+ 
     <div>
+          <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg"/>
+      <h1> My name Hospital </h1>
       
       <MedicalTreatmentPanel/>
  
--- src/MedicalTreatmentPanel.js
+++ src/MedicalTreatmentPanel.js
@@ -17,6 +17,7 @@ function MedicalTreatmentPanel() {
   const [name, setName] = React.useState('');
 
   const [startDate, setStartDate] = React.useState('');
+  const [display, setDisplay] = React.useState();
 
 
   const handleTreatChange = e => setTreatId(e.target.value)
@@ -25,14 +26,31 @@ function MedicalTreatmentPanel() {
   const handleCategoryChange = e => setCategory(e.target.value)
   const handleNameChange = e => setName(e.target.value)
   const handleStartDatechange = e => setStartDate(e.target.value)
-
-
-  function showClickHandler () {
+  const handleDisplay = e => setDisplay("hello")
   
+
+
+  function ShowClickHandler () {
+    if(display === "hello"){
+      return(
+      <h1> 
+
+<li>{treatId}</li>
+<li>{treatCourseId}</li>
+<li>{type}</li>
+<li>{category}</li>
+<li>{name}</li>
+<li>{startDate}</li>
+
+
+      </h1> 
+      )
+    }
+    return null;
 
   
       
-    
+   
    
 }
 
@@ -44,6 +62,7 @@ function clearClickHandler () {
     setCategory('');
     setName('');
     setStartDate('');
+    setDisplay();
 
 }
 
@@ -74,20 +93,12 @@ function clearClickHandler () {
 
         <input placeholder="startDate" value={startDate}  onChange = {handleStartDatechange}  ></input>
 
-        <button style={{color:"blue"}} onClick={showClickHandler}>show</button>
+        <button style={{color:"blue"}} onClick={handleDisplay}>show</button>
 
       <button style={{color:"red"}} onClick={clearClickHandler}>Clear</button>
-        <h1> 
-          
-          {treatId}
-          {treatCourseId}
-          {type}
-          {category}
-          {name}
-          {startDate}
-    
-          </h1> 
+      < ShowClickHandler />
         </div>
+    
     );
 
   }
