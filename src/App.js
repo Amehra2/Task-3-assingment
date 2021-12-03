9e06394855e3671d9a383a853135c5b6596cd52f
Amehra2 <akashmehra2001@hotmail.com>
2021-12-03 13:31:43 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./style.css";
-import MedicalTreatmentPanel from "./MedicalTreatmentPanel";
+import MedicalTreatmentPanel from "./MedicalTreatmentPanel"; //Here I have imported the MedicalTreatmentPanel component to the app.js
 
 export default function App() {
   return (
 
- 
+ //This here below will show the title of the panel, which I have named as LHM hosital. Below that I have alo retrieved the data that we included in the MedicalTreatmentPanel.
     <div>
-       <h1> LHM Hospital </h1>
+       <h1> LHM Hospital </h1> 
 
 
     <MedicalTreatmentPanel/>
